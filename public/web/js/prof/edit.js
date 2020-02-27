74464a1db49d50cd0f9b06bc2aee4e666b8971d4
msdevsofts <m.s.dev.softs@gmail.com>
2020-02-28 01:22:53 +0900

    プロフ画面 パスワード編集切り替え処理作成

--- public/web/js/prof/edit.js
+++ public/web/js/prof/edit.js
@@ -1,17 +1,25 @@
 $(function() {
     $('.edit').click(function() {
-        let nicknameSpan = $(this).parents('p').find('.nickname');
-        let nicknameInput = $('#nickname');
-        if (nicknameSpan.css('display') === 'none') {
-            nicknameSpan.css('display', 'inline-block');
-            nicknameInput.attr('type', 'hidden');
-            let nickname = nicknameInput.val().isEmpty() ? Default.NICKNAME : nicknameInput.val();
-            nicknameSpan.text(nickname);
+        let container = $(this).parents('.container');
+        if (container.hasClass('nickname')) {
+            let nicknameSpan = $(this).parents('p').find('.nickname');
+            let nicknameInput = $('#nickname');
+            if (nicknameSpan.css('display') === 'none') {
+                nicknameSpan.css('display', 'inline-block');
+                nicknameInput.attr('type', 'hidden');
+                let nickname = nicknameInput.val().isEmpty() ? Default.NICKNAME : nicknameInput.val();
+                nicknameSpan.text(nickname);
+            }
+            else {
+                nicknameSpan.css('display', 'none');
+                nicknameInput.attr('type', 'text');
+                nicknameInput.removeAttr('style');
+            }
         }
-        else {
-            nicknameSpan.css('display', 'none');
-            nicknameInput.attr('type', 'text');
-            nicknameInput.removeAttr('style');
+        else if (container.hasClass('password')) {
+            $(this).css('display', 'none');
+            $(this).parents('p').find('.password').css('display', 'none');
+            $('#password').removeClass('hide').addClass('text-520');
         }
         return false;
     });
--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -7,14 +7,14 @@
         </a>
     </div>
 
-    <div>
+    <div class="prof">
         <div class="container border_bottom">
             <p class="clear no_spacing">
                 <span class="text-120">ユニークID</span>
                 {{ $uniqueId ?? '' }}
             </p>
         </div>
-        <div class="container border_bottom">
+        <div class="container border_bottom nickname">
             <p class="clear no_spacing">
                 <label for="nickname" class="text-120 left">ニックネーム</label>
                 <span class="nickname text-520 left text-smaller">
@@ -24,16 +24,14 @@
                 <span class="edit left"><a href="">編集...</a></span>
             </p>
         </div>
-        <div class="container border_bottom">
+        <div class="container border_bottom password">
             <p class="clear no_spacing">
                 <span class="text-120 left">パスワード</span>
                 <span class="password left text-520 text-smaller">設定されたパスワード</span>
-                <span class="left hide">
-                    <span>
-                        <input type="text" name="password" id="" class="text-520" placeholder="パスワード">
-                        <input type="text" name="new_pw" id="" class="text-520" placeholder="新しいパスワード">
-                        <input type="text" name="new_pw_confirm" class="text-520" id="新しいパスワード（確認）">
-                    </span>
+                <span id="password" class="left hide">
+                    <input type="text" name="password" id="" class="text-520" placeholder="パスワード">
+                    <input type="text" name="new_pw" id="" class="text-520" placeholder="新しいパスワード">
+                    <input type="text" name="new_pw_confirm" class="text-520" placeholder="新しいパスワード（確認）">
                 </span>
                 <span class="edit left"><a href="">編集...</a>
             </p>
