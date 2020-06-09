eaef7a6290f5fe316200434fdd348726eb9ca56a
msdevsofts <m.s.dev.softs@gmail.com>
2020-06-10 01:17:24 +0900

    メッセージ 一覧取得クエリを修正

--- app/Services/MessageListService.php
+++ app/Services/MessageListService.php
@@ -5,6 +5,7 @@ namespace App\Services;
 
 
 use App\MessageList;
+use Illuminate\Support\Facades\DB;
 
 class MessageListService
 {
@@ -17,19 +18,30 @@ class MessageListService
 
     public function getMessageList() {
         return MessageList::join('message_members', 'message_members.id', '=', 'message_lists.id')
-            ->join('messages', 'messages.message_list_id', '=', 'message_lists.id')
+            ->joinSub(
+                function ($query) {
+                    $query->select([
+                            DB::raw('distinct(message_list_id)'),
+                            'message',
+                            'created_at'
+                        ])
+                        ->from('messages')
+                        ->where(['member_id' => $this->memberId])
+                        ->whereNotNull('removed_at');
+                },
+                'latest_messages',
+                'latest_messages.message_list_id',
+                '=',
+                'message_lists.id'
+            )
             ->where(['message_members.member_id' => $this->memberId])
-            ->groupBy('message_lists.id')
-            ->orderBy('messages.created_at', 'desc')
+            ->orderBy('latest_messages.created_at', 'desc')
             ->orderBy('message_lists.created_at', 'desc')
-            ->toSql();
-/*
             ->get([
                 'message_lists.id',
                 'message_lists.name',
-                'messages.message'
+                'latest_messages.message'
             ])
             ->toArray();
-*/
     }
 }
